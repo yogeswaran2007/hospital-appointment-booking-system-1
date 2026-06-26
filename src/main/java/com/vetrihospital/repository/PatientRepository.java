d8bc4f9a3691db85ffac23cc61c3422640db5efe
Yogeswaran <yogesvetri2007@gmail.com>
2026-06-26 11:22:31 +0530

    Create src/main/java/com/vetrihospital/repository/PatientRepository.java

---
+++ src/main/java/com/vetrihospital/repository/PatientRepository.java
@@ -0,0 +1,16 @@
+package com.vetrihospital.repository;
+
+import com.vetrihospital.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByEmail(String email);
+
+    Optional<Patient> findByPhone(String phone);
+
+}
