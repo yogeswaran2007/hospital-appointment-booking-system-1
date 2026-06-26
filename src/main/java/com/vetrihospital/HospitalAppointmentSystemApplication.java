4b1f3c41e36ea5534d09c97062bcf8871c957d46
Yogeswaran <yogesvetri2007@gmail.com>
2026-06-26 11:12:13 +0530

    Create src/main/java/com/vetrihospital/HospitalAppointmentSystemApplication.java

---
+++ src/main/java/com/vetrihospital/HospitalAppointmentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.vetrihospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAppointmentSystemApplication.class, args);
+    }
+}
